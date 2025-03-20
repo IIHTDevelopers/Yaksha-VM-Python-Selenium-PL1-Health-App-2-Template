12843e071d54dc8e5c8a9a38799c42647bf8f2fc
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-20 15:51:37 +0530

    Delete tests directory

--- tests/TestResults.py
+++
@@ -1,7 +0,0 @@
-import json
-class TestResults(dict):
-    testCaseResults = ""
-    customData = ""
-
-    def __init__(self, testCaseResults, customData):
-        dict.__init__(self, testCaseResults = testCaseResults, customData = customData)
